684c09c6b4b3bd424cdd61881221d7f7e3dc8372
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-05 00:41:43 +0300

    Infinite Queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,28 +9,31 @@ interface Post {
   }
 
 interface PostQuery {
-  page: number;
   pageSize: number
 }
 
 const usePosts = (query: PostQuery) => {
-    const fetchPosts = () =>
+    const fetchPosts = ( { pageParam = 1 }: any ) =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts",{
         params: {
-          _start: (query.page - 1) * query.pageSize, //Utilizatorul vede pagina 1, nu 0 cum e in programare
+          _start: (pageParam - 1) * query.pageSize, //Utilizatorul vede pagina 1, nu 0 cum e in programare
           _limit: query.pageSize
         }
       })
       .then((res) => res.data);
 
-    return useQuery<Post[], Error>({
+    return useInfiniteQuery<Post[], Error>({
       //like in URL /user/1/posts
         queryKey: ["posts", query],  //Simmillar to dependency array to Effect hook
         //Avem carnatul asta ca sa nu avem cheia urata: [users, null, posts]
-        queryFn: fetchPosts,
+        queryFn: (parameter) => fetchPosts(parameter),
         staleTime: 1 * 60 * 1000, //1 minute
-        placeholderData: keepPreviousData //This keeps the previos data untill the new one arrives
+        placeholderData: keepPreviousData, //This keeps the previos data untill the new one arrives
+        initialPageParam: undefined,
+        getNextPageParam: (lastPage, allPages) => {
+          return lastPage.length > 0 ? allPages.length + 1 : undefined; //Mock backend sends empty array as "lastPage" if it is out
+        }
       });
 }
 
