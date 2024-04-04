d9bc6ef1d09d587ffa57b079703250bfa319d2b7
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-04 19:18:06 +0300

    Handling Errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 const queryClient = new QueryClient();
 
