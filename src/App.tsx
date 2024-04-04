a526f0df5fbd2c7302ef9f1b61b6e32a93482eb2
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-04 23:32:11 +0300

    Exercise-Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <TodoList></TodoList>;
+  return (
+    <>
+      <div className="mb-5">
+        <h1>TodoList</h1>
+        <TodoList></TodoList>
+      </div>
+      <div>
+        <h1>PostList</h1>
+        <PostList></PostList>
+      </div>
+    </>
+  );
 }
 
 export default App;
