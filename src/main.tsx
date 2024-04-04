9f2b9cfd46931e5a3e87f4c95afb43cf6b9048bc
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-04 23:21:27 +0300

    Customizing Query Settings

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,21 @@ import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
+//global options if needed
 const queryClient = new QueryClient();
+//  {
+//  defaultOptions: {
+//    queries: {
+//      retry: 3, //It is the default value, but here we use it as an example
+//      gcTime: 300_000, //5 minutes default, used to be called cacheTime
+//      staleTime: 10 * 1000, //10 seconds now, 0 default
+//      //If stale => autorefresh when network reconnected || when a component is mounted || when the window is refocused
+//      refetchOnWindowFocus: true, //default true
+//      refetchOnReconnect: true, //default true
+//      refetchOnMount: true, //default true
+//    },
+//  },
+//}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
