df1435cd9bd2323cfc038ca2db6c2c04914f5ce5
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-04 23:48:48 +0300

    Parameterized Queries meaning Querry Params

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,7 @@ import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return (
-    <>
-      <div className="mb-5">
-        <h1>TodoList</h1>
-        <TodoList></TodoList>
-      </div>
-      <div>
-        <h1>PostList</h1>
-        <PostList></PostList>
-      </div>
-    </>
-  );
+  return <PostList></PostList>;
 }
 
 export default App;
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,14 +8,20 @@ interface Post {
     userId: number;
   }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
     const fetchPosts = () =>
     axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts",{
+        params: {
+          userId
+        }
+      })
       .then((res) => res.data);
 
     return useQuery<Post[], Error>({
-        queryKey: ["posts"],
+      //like in URL /user/1/posts
+        queryKey: userId ? ["users", userId, "posts"] : ["posts"],  //Simmillar to dependency array to Effect hook
+        //Avem carnatul asta ca sa nu avem cheia urata: [users, null, posts]
         queryFn: fetchPosts,
         staleTime: 1 * 60 * 1000 //1 minute
       });
