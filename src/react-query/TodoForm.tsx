66c589fc71d482c3b96ae24894881558548cbc79
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-05 21:16:05 +0300

    Creating a Custom Mutation Hook

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,65 +1,11 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRef } from "react";
-import { Todo } from "./hooks/useTodos";
-import axios from "axios";
-
-interface AddTodoContext {
-  previousTodos: Todo[];
-}
+import useAddTodo from "./hooks/useAddTodo";
 
 const TodoForm = () => {
-  const queryClient = useQueryClient();
-
-  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
-    // <What we get from server, The error type class(shoudn't change), What we sent the server>
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-        .then((res) => res.data),
-
-    onMutate: (newTodo: Todo) => {
-      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || []; //Make a save for optimistic update to revert if error
-
-      //Function that is called before the mutation is executed
-      //APPROACH_3 Like APPROACH_2 but with optimistic update
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        newTodo,
-        ...(todos || []), //Should also work with immer
-      ]);
-
-      if (ref.current) ref.current.value = "";
-
-      return { previousTodos }; //Return previos so we will have context in Error function if necessary
-    },
-
-    onSuccess: (savedTodo, newTodo) => {
-      //newTodo is what we sent, savedTodo is what we recieved
-
-      //APPROACH_1: Invalidating the cache --> NOTE IT DOES NOT WORK WITH MOCK BACKEND CUZ IT JUST SENDS US THE SAME EVERYTIME
-      //queryClient.invalidateQueries({
-      //  queryKey: ["todos"], //This invalidates all queries that start with todos
-      //});
-
-      //APPROACH_2: Updating the cache directly
-      //queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-      //  savedTodo,
-      //  ...(todos || []), //Should also work with immer
-      //]);
-      //if (ref.current) ref.current.value = "";
-
-      //APPROACH_3
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
-        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
-      );
-    },
-    onError: (error, newTodo, context) => {
-      //newTodo is the new object we try to create/update, context
-      if (!context) return; //First querry failed. We got no data. There is nothing todo cuz all failed
-
-      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos); //Revert back to previous "cache"
-    },
-  });
   const ref = useRef<HTMLInputElement>(null);
+  const addTodo = useAddTodo(() => {
+    if (ref.current) ref.current.value = "";
+  });
 
   return (
     <>
---
+++ src/react-query/hooks/useAddTodo.ts
@@ -0,0 +1,64 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Todo } from "./useTodos";
+import axios from "axios";
+
+interface AddTodoContext {
+    previousTodos: Todo[];
+  }
+
+const useAddTodo = (onAdd: () => void) => {
+    const queryClient = useQueryClient();
+
+  return useMutation<Todo, Error, Todo, AddTodoContext>({
+    // <What we get from server, The error type class(shoudn't change), What we sent the server>
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || []; //Make a save for optimistic update to revert if error
+
+      //Function that is called before the mutation is executed
+      //APPROACH_3 Like APPROACH_2 but with optimistic update
+      queryClient.setQueryData<Todo[]>(["todos"], (todos = []) => [ //If undefined it will become [] and makes the bellow code cleaner
+        newTodo,
+        ...todos, //Should also work with immer
+      ]);
+
+      //if (ref.current) ref.current.value = ""; We don't update directly anymore the UI, we should give the component a callback function
+      onAdd();  //This is the callback function
+
+      return { previousTodos }; //Return previos so we will have context in Error function if necessary
+    },
+
+    onSuccess: (savedTodo, newTodo) => {
+      //newTodo is what we sent, savedTodo is what we recieved
+
+      //APPROACH_1: Invalidating the cache --> NOTE IT DOES NOT WORK WITH MOCK BACKEND CUZ IT JUST SENDS US THE SAME EVERYTIME
+      //queryClient.invalidateQueries({
+      //  queryKey: ["todos"], //This invalidates all queries that start with todos
+      //});
+
+      //APPROACH_2: Updating the cache directly
+      //queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+      //  savedTodo,
+      //  ...(todos || []), //Should also work with immer
+      //]);
+      //if (ref.current) ref.current.value = "";
+
+      //APPROACH_3
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      //newTodo is the new object we try to create/update, context
+      if (!context) return; //First querry failed. We got no data. There is nothing todo cuz all failed
+
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos); //Revert back to previous "cache"
+    },
+  });
+}
+
+export default useAddTodo;
