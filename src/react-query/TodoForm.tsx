6625412cd5c26f6cf852f2c3b05c5f982b83a365
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-05 21:03:52 +0300

    Optimistic Updates

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,15 +3,35 @@ import { useRef } from "react";
 import { Todo } from "./hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     // <What we get from server, The error type class(shoudn't change), What we sent the server>
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || []; //Make a save for optimistic update to revert if error
+
+      //Function that is called before the mutation is executed
+      //APPROACH_3 Like APPROACH_2 but with optimistic update
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []), //Should also work with immer
+      ]);
+
+      if (ref.current) ref.current.value = "";
+
+      return { previousTodos }; //Return previos so we will have context in Error function if necessary
+    },
+
     onSuccess: (savedTodo, newTodo) => {
       //newTodo is what we sent, savedTodo is what we recieved
 
@@ -21,12 +41,22 @@ const TodoForm = () => {
       //});
 
       //APPROACH_2: Updating the cache directly
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
-        ...(todos || []), //Should also work with immer
-      ]);
+      //queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+      //  savedTodo,
+      //  ...(todos || []), //Should also work with immer
+      //]);
+      //if (ref.current) ref.current.value = "";
 
-      if (ref.current) ref.current.value = "";
+      //APPROACH_3
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      //newTodo is the new object we try to create/update, context
+      if (!context) return; //First querry failed. We got no data. There is nothing todo cuz all failed
+
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos); //Revert back to previous "cache"
     },
   });
   const ref = useRef<HTMLInputElement>(null);
