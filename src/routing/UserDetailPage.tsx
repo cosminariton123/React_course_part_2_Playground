259985efe4c63f2ec68fde9b8b8af39bc689aa39
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-15 23:17:25 +0300

    Getting Data about the Current Route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,16 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  console.log(params);
+  console.log(searchParams.toString()); //Put in link a querry parameter with name and age
+  console.log(searchParams.get("name"));
+
+  const location = useLocation();
+  console.log(location);
+
   return <p>User</p>;
 };
 
