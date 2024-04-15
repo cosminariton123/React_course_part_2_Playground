3e03501ba74a1fa5b65522fc2c79aa813609f2f2
cosminariton123 <cosmin.ariton@gmail.com>
2024-04-15 23:04:52 +0300

    Navigation

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
+      <Link to="/users">
+        Users with Link from react-router-dom that doesn't use a full page
+        reload
+      </Link>
+      <br></br>
       <a href="/users">Users</a>
     </>
   );
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
+import ContactPage from "./ContactPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage></HomePage> },
   { path: "/users", element: <UserListPage></UserListPage> },
+  { path: "/contact", element: <ContactPage></ContactPage> },
 ]);
 
 export default router;
